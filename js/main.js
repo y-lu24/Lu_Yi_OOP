a9b3065a51fa287b880466f7b2cb61917d271359
y-lu24 <yilu2559@gmail.com>
2026-03-14 09:57:58 -0400

    Add fourth counter and update counter logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import { Counter } from './modules/counter.js';
+import { Counter, StepCounter } from './modules/counter.js';
 
 const counter1 = new Counter("#counter-container1");
-const counter2 = new Counter('#counter-container2');
-const counter3 = new Counter ('#counter-container3');
+const counter2 = new Counter('#counter-container2', 5);
+const counter3 = new Counter('#counter-container3', 10);
+const counter4 = new StepCounter('#counter-container4', 0, 5);
